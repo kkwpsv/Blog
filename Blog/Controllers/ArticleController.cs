f12450cd5bfdfc9f27e117d2fb0847e606290b8e
Shaojun Li <i@shaojun.li>
2019-03-17 05:14:55 +0800

    Article Page and Optimize Index Page

---
+++ Blog/Controllers/ArticleController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Blog.Models;
+
+namespace Blog.Controllers
+{
+    public class ArticleController : BaseController
+    {
+        public ArticleController(DataContext dataContext) : base(dataContext)
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
