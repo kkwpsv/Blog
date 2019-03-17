a059ddae307c98ec28c37709e1c6524b1c6ae04c
Shaojun Li <i@shaojun.li>
2019-03-17 16:19:29 +0800

    Admin Layout

---
+++ Blog/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Blog.Models;
+
+namespace Blog.Controllers
+{
+    public class AdminController : BaseController
+    {
+        public AdminController(DataContext dataContext) : base(dataContext)
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
